cd2627409faa55b0092feea2f8feccb9e5c9e48e
Valentín Veintemilla <valentinveintemilla@gmail.com>
2024-05-20 17:08:10 -0300

    Last changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 allFigures = document.querySelector("#main").querySelectorAll("figure");
+
+if (allFigures.length % 2 != 0){
+    allFigures[allFigures.length-1].classList.add("col-xl-9");
+    allFigures[allFigures.length-1].classList.remove("col-xl-6");
+}
